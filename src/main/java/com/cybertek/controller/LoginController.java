85402adcb656d8ca411dfdb29e6e97cd0e6335e2
karinatech <karinaminty@gmail.com>
2021-06-13 18:23:13 -0400

    minor fixes

--- src/main/java/com/cybertek/controller/LoginController.java
+++ src/main/java/com/cybertek/controller/LoginController.java
@@ -1,40 +1,87 @@
 package com.cybertek.controller;
 
 
+import com.cybertek.annotation.DefaultExceptionMessage;
+import com.cybertek.cnverters.MapperUtil;
+import com.cybertek.dto.UserDTO;
 import com.cybertek.entity.AuthenticationRequest;
 import com.cybertek.entity.ResponseWrapper;
+import com.cybertek.entity.Role;
+import com.cybertek.entity.User;
+
+import com.cybertek.exception.TicketingProjectException;
+import com.cybertek.implementations.SecurityServiceImplementation;
+import com.cybertek.service.RoleService;
+import com.cybertek.service.SecurityService;
+import com.cybertek.service.UserService;
+import com.cybertek.util.JwtUtil;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 public class LoginController {
 
+private AuthenticationManager authenticationManager;
 
-    @PostMapping("/authenticate")
-    public ResponseEntity<ResponseWrapper> doLogin(@RequestBody AuthenticationRequest authenticationRequest) {
+    private UserService userService;
+    private MapperUtil mapperUtil;
+    private JwtUtil jwtUtil;
+    private SecurityServiceImplementation securityServiceImplementation;
+    private RoleService roleService;
+
+    public LoginController(AuthenticationManager authenticationManager, UserService userService, MapperUtil mapperUtil, JwtUtil jwtUtil, SecurityServiceImplementation securityServiceImplementation, RoleService roleService) {
+        this.authenticationManager = authenticationManager;
+        this.userService = userService;
+        this.mapperUtil = mapperUtil;
+        this.jwtUtil = jwtUtil;
+        this.securityServiceImplementation = securityServiceImplementation;
+        this.roleService = roleService;
+    }
+
+    @RequestMapping("/hello")
+    public ResponseEntity<ResponseWrapper> read(){
+        List<User> result = userService.listAllUsers();
+        return ResponseEntity.ok(new ResponseWrapper("Successfully retrieved users",result));
+    }
+
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    public ResponseEntity<?>createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
 
         String password = authenticationRequest.getPassword();
         String username = authenticationRequest.getUsername();
 
-        return ResponseEntity.ok(new ResponseWrapper("Login Successful", ));
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(username,password);
+        authenticationManager.authenticate(authentication);
 
+
+        final UserDetails userDetails = securityServiceImplementation
+                .loadUserByUsername(authenticationRequest.getUsername());
+
+        final String jwt = jwtUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new ResponseWrapper("Login Successful",jwt));
+
+   }
+    @GetMapping("/users")
+    @DefaultExceptionMessage(defaultMessage = "Something went wrong, try again!")
+    @PreAuthorize("hasAuthority('Admin')")
+    public ResponseEntity<ResponseWrapper> readAll(){
+        List<User> result = userService.listAllUsers();
+        return ResponseEntity.ok(new ResponseWrapper("Successfully retrieved users",result));
     }
-
-@GetMapping("/layout-horizontal")
-    public String changeLayout(){
-    return "html/layout-horizontal-dark";
-}
-
-@GetMapping("/data-table")
-    public String getDataTable(){
-    return "html/table-datatable";
-}
-
-    @GetMapping("/basic-form")
-    public String getForm(){ return "html/form-basic"; }
+//    @PostMapping("/create-user")
+//    @DefaultExceptionMessage(defaultMessage = "Something went wrong, try again!")
+//    @PreAuthorize("hasAuthority('Admin')")
+//    public ResponseEntity<ResponseWrapper> doRegister(@RequestBody UserDTO userDTO) throws TicketingProjectException {
+//        UserDTO createdUser = userService.save(userDTO);
+//        return ResponseEntity.ok(new ResponseWrapper("User has been created!",createdUser));
+//    }
 
 
 
